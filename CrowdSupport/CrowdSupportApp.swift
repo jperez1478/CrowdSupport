5f68d97fcbdf385082fce6157345dabd04392018
jperez1478 <jessicperez3@gmail.com>
2022-04-19 12:54:22 -0500

    Initial Commit

---
+++ CrowdSupport/CrowdSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdSupportApp.swift
+//  CrowdSupport
+//
+//  Created by Jessica Perez on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CrowdSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
