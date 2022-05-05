59b388afb14fbbb433bdd9ae1655a284724a8497
jperez1478 <jessicperez3@gmail.com>
2022-05-05 10:04:11 -0500

    up to date stop off

--- CrowdSupport/ContentView.swift
+++ CrowdSupport/ContentView.swift
@@ -6,24 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     //MARK: -Property
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
-    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.50722, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     //MARK: BODY
     var body: some View {
-        ZStack {
-            VStack(spacing: 20) {
-                Text("Onboarding")
-                    .font(.largeTitle)
+        Map(coordinateRegion:  $region)
+            .frame(width: 400, height: 300)
                 
             }
         }
        
-       
-    }
-}
+     
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
